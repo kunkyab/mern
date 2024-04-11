f5fdda526c27e8c0300b44f58af4b7c745e8b869
tenzinkunkyab <tenzinkunkyab5@gmail.com>
2024-04-10 20:35:43 -0400

    changesin server.js added cors

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const app = express();
 
 const PORT = 5000;
 
-
+app.use(cors({
+origin:["https://deploy-mern-1whq.vercel.app"],
+methods:["POST","GET"],
+credentials:true,
+}));
 app.get('/',(req,res)=>{
 
   res.send("<h2>free tibet</h2>")
