6d1c6bb7619ef22bbdfe009bd816270dbcaf2457
tenzinkunkyab <tenzinkunkyab5@gmail.com>
2024-04-10 21:07:40 -0400

    changes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,7 @@ const app = express();
 
 const PORT = 5000;
 
-app.use(cors({
-origin:["https://mern-server-ebpmzq5ql-tenzinkunkyabs-projects.vercel.app/"],
-methods:["POST","GET"],
-credentials:true,
-}));
+
 app.get('/',(req,res)=>{
 
   res.send("<h2>free tibet</h2>")
